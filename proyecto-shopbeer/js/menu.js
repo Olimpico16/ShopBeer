cc6e788bd551d62a6a60a89603a4c6ba76cf890d
Olimpico <95925621+Olimpico16@users.noreply.github.com>
2022-06-22 19:36:02 -0500

    Add files via upload

---
+++ proyecto-shopbeer/js/menu.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById('menu');
+let btnMenu = document.getElementById('btnmenu');
+
+btnMenu.addEventListener('click', function() {
+    'use strict';
+    menu.classList.toggle('mostrar')
+})
